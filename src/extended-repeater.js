826764e6b81bdda403719d7943bec383b6d13398
MRKS77 <artxmarkos@gmail.com>
2021-09-12 16:31:59 +0300

    feat: implement repeater and hanoi-tower

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,20 @@
-import { NotImplementedError } from '../extensions/index.js';
+export default function repeater(str, options) {
+  if (typeof str !== 'string') str = String(str)
 
-/**
- * Create a repeating string based on the given parameters
- *  
- * @param {String} str string to repeat
- * @param {Object} options options object 
- * @return {String} repeating string
- * 
- *
- * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let add = '', string = str
+
+  if (!(options.addition === undefined)) {
+    add += options.addition
+    if (typeof add !== 'string') add = String(add)
+  }
+  add += options.additionSeparator ? options.additionSeparator : '|'
+  if (options.additionRepeatTimes) add = add.repeat(options.additionRepeatTimes)
+  add = add.slice(0, add.length - (options.additionSeparator ? options.additionSeparator.length : 1))
+
+  if (add) string += add
+  string += options.separator ? options.separator : '+'
+  if (options.repeatTimes) string = string.repeat(options.repeatTimes)
+  string = string.slice(0, string.length - (options.separator ? options.separator.length : 1))
+
+  return string
 }
